0c786f3cb3efec8426a0ef599bbfd8cbd1e3b582
ebertmota <ebert.mota03@gmail.com>
2021-02-02 21:42:22 -0300

    supporting png images

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
-const withReactSvg = require('next-react-svg');
-const path = require('path');
+const withImages = require('next-images');
 
-module.exports = withReactSvg({
-  include: path.resolve(__dirname, 'src/assets/svg'),
-  webpack(config, options) {
-    return config;
-  },
+module.exports = withImages({
+  esModule: true,
 });
